5522b28eb3b5d0b125a86d6b09e7e452c86b498f
aliaksandr vishneuski <aliaksandr_vishneuski@senla.eu>
2019-08-14 14:14:38 +0300

    comment

---
+++ src/Car/Car.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+//1
+// function Car(){
+//     return (
+//         <div>This is react component </div>
+//     )
+// }
+//
+//export default Car
+
+
+//2.
+// const Car = () => {
+//     return (
+//         <div>This is react component </div>
+//     )
+// } 
+//
+//export default Car
+
+//3.
+// const Car = () => <div>This is react component 3 </div>
+//
+// export default Car
+
+export default () => <div>This is react component 4 </div>
