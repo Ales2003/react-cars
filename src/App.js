b1166798b7d7f7352ccd99698a7d5c5d7669ab3a
aliaksandr vishneuski <aliaksandr_vishneuski@senla.eu>
2019-08-15 12:31:06 +0300

    Lifecycle basic

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import Car from  './Car/Car';
 class App extends Component {
 
   constructor(props){
+    console.log('App constructor1');
     super(props)
-
+    console.log('App constructor2');
     this.state = {
       cars: [
         {name: 'Ford', year: 2012},
@@ -56,11 +57,22 @@ class App extends Component {
     this.setState({
       cars
     })
-
   }
 
+  UNSAFE_componentWillMount(){
+    console.log('App componentWillMount - before render. вызывается когда произошла инициализация, параметры подготогвлены и готовы чтобы их положили в дом дерево. Нельзя изменять state т.к. html не готов. нел недо обращаться к бэку')
+  }
+
+  componentDidMount(){
+    console.log('App componentDidMount - after render. Весь html уже готов и можно с ним работать ')
+  }
+
+  // componentWillUpdate(){
+  //   console.log('App componentWillUpdate - before render')
+  // }
+
   render (){
-    console.log('in title change');
+    console.log('App render - put in DOM-tree. ю формирует конечный jsx к-й будет преобразован в видке html');
     const divStyle = {
       textAlign: 'center',
     }
